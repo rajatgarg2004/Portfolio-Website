959ac3fda2504df86f8e2875fb0361da330cb4e0
rajatgarg2004 <rajatgarg.bt21ele@pec.edu.in>
2024-04-21 00:44:38 +0530

    Static Remove

--- front-end/src/Components/Footer.js
+++ front-end/src/Components/Footer.js
@@ -2,12 +2,12 @@ export default function Footer({footerColor}){
     return(
         <footer className={`bg-${footerColor} p-6`}>
             <ul className="flex flex-wrap justify-between ">
-                <li><a href="https://www.linkedin.com/in/rajat-garg-72b15a226" target="_blank" rel="noreferrer"><img src="/static/SVG/Linkedin.svg" alt="LINKEDIN" /></a></li>
-                <li><a href="https://www.github.com/rajatgarg2004" target="_blank" rel="noreferrer"><img src="/static/SVG/Github.svg" alt="GITHUB" /></a></li>
-                <li><a href="https://www.leetcode.com/rajatgarg2004" target="_blank" rel="noreferrer"><img src="/static/SVG/Leetcode.svg" alt="LEETCODE" /></a></li>
-                <li><a href="https://www.codechef.com/users/yeetgod68" target="_blank" rel="noreferrer"><img src="/static/SVG/Codechef.svg" alt="CODECHEF" /></a></li>
-                <li><a href="https://www.hackerrank.com/rajatgarg2004" target="_blank" rel="noreferrer"><img src="/static/SVG/Hackerrank.svg" alt="HACKERRANK" /></a></li>
-                <li><a href="https://www.naukri.com/code360/profile/8d636cc8-19b7-4110-bca4-0dde587737c6" target="_blank" rel="noreferrer"><img src="/static/SVG/CodingNinjas.svg" alt="CodingNinjas" /></a></li>
+                <li><a href="https://www.linkedin.com/in/rajat-garg-72b15a226" target="_blank" rel="noreferrer"><img src="/SVG/Linkedin.svg" alt="LINKEDIN" /></a></li>
+                <li><a href="https://www.github.com/rajatgarg2004" target="_blank" rel="noreferrer"><img src="/SVG/Github.svg" alt="GITHUB" /></a></li>
+                <li><a href="https://www.leetcode.com/rajatgarg2004" target="_blank" rel="noreferrer"><img src="/SVG/Leetcode.svg" alt="LEETCODE" /></a></li>
+                <li><a href="https://www.codechef.com/users/yeetgod68" target="_blank" rel="noreferrer"><img src="/SVG/Codechef.svg" alt="CODECHEF" /></a></li>
+                <li><a href="https://www.hackerrank.com/rajatgarg2004" target="_blank" rel="noreferrer"><img src="/SVG/Hackerrank.svg" alt="HACKERRANK" /></a></li>
+                <li><a href="https://www.naukri.com/code360/profile/8d636cc8-19b7-4110-bca4-0dde587737c6" target="_blank" rel="noreferrer"><img src="/SVG/CodingNinjas.svg" alt="CodingNinjas" /></a></li>
             </ul>
         </footer>
     );
